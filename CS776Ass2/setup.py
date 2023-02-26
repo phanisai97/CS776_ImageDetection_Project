30ab579ae14621274dd3199dd6de271275d1abc0
phanisai97 <110308067+phanisai97@users.noreply.github.com>
2023-02-27 00:50:21 +0530

    Added CookieCutter style Project Management

---
+++ CS776Ass2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project data structure deals with cs776 2023 assignment 2',
+    author='gnanendrachannamsetty',
+    license='MIT',
+)
